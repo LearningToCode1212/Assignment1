a227050b05b6603c0f9586794741aca6f2ae4128
LearningToCode1212 <124621733+LearningToCode1212@users.noreply.github.com>
2023-12-07 11:14:05 +0200

    Finished Assignment 1

---
+++ Assignment1Sln/Assignment1/MilkSong.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class MilkSong
+    {
+        public static string Milk()
+        {
+            for (int i = 99; i > 0; i--)
+            {
+                if (i == 1)
+                {
+                    Console.WriteLine($"{i} bottle of milk on the wall, {i} bottle of milk.");
+                    break;
+                }
+
+                Console.WriteLine($"{i} bottles of milk on the wall, {i} bottles of milk.");
+                Console.WriteLine($"Take one down and past it around, {i - 1} bottles of milk.");
+            }
+            return "";
+        }
+    }
+}
--- Assignment1Sln/Assignment1/Program.cs
+++ Assignment1Sln/Assignment1/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string milksong = MilkSong.Milk();
+            Console.WriteLine(milksong);
         }
     }
 }
